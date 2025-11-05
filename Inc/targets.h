d61592d6ffb0c13dcd2473f1d082a33512c638a3
djmorvay <djmorvay@me.com>
2025-11-05 10:27:26 -0500

    Adding v2.19 for single ESC

--- Inc/targets.h
+++ Inc/targets.h
@@ -2582,6 +2582,18 @@
 #define SIXTY_FOUR_KB_MEMORY
 #endif
 
+#ifdef LMNR_SIEGE_100A8S_SINGLE_G071
+#define FILE_NAME "LMNR_SIEGE_100A8S_SINGLE_G071"
+#define FIRMWARE_NAME "LSS_100A8S"
+#define DEAD_TIME 25
+#define MILLIVOLT_PER_AMP 24
+#define CURRENT_OFFSET 0
+#define TARGET_VOLTAGE_DIVIDER 130
+#define HARDWARE_GROUP_G0_A
+#define USE_SERIAL_TELEMETRY
+#define SIXTY_FOUR_KB_MEMORY
+#endif
+
 /********************************** F031 Targets
  * ***********************************/
 
